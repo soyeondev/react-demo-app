a1c5c07b6cf614df204c1c5452406a58475807a9
soyeon <soyeon.dev@gmail.com>
2020-07-27 10:42:00 +0900

    mode change and anchor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import TOC from "./components/TOC";
 import Subject from "./components/Subject";
 import Content from "./components/Content";
+import Control from "./components/Control";
 
 import './App.css';
 
@@ -11,11 +12,12 @@ class App extends Component {
     this.state = {
       //mode: 'welcome',
       mode: 'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, react!'},
       contents:[
         {id:1, title: 'HTML', desc:'HTML is HyperText ..'},
-        {id:2, title: 'CSS', desc:'CSS is ..'},
+        {id:2, title: 'CSS', desc:'CSS is for design!!'},
         {id:3, title: 'JavaScript', desc:'JavaScript is ..'},
       ]
     }
@@ -27,28 +29,42 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
 
 
     return (
       <div className="App">
-        {/* <Subject title={this.state.subject.title} 
-                 sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            //this.state.mode = 'welcome';
-             this.setState({
-               mode: 'welcome'
-             });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-              {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
+        <Subject title={this.state.subject.title} 
+                 sub={this.state.subject.sub}
+                 onChangePage={function(){
+                  this.setState({mode:'welcome'});
+                }.bind(this)}>
+                   
+        </Subject>
+        <TOC 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+        }.bind(this)} 
+        data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );    
