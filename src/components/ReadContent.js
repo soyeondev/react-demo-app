4f69e4bf1106db7d68a576fbf36c679fa88dcc7b
soyeon <soyeon.dev@gmail.com>
2020-07-28 16:51:06 +0900

    complete demo project

--- src/components/ReadContent.js
+++ src/components/ReadContent.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class ReadContent extends Component {
     render (){
-      console.log('Content render')
+      console.log('ReadContent render')
       return (
         <article>
           <h2>{this.props.title}</h2>
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 
 class TOC extends Component {
+    shouldComponentUpdate(newProps, newState){
+      // 성능을 더 높어야하는 상황이 아니라면 shouldComponentUpate를 사용하지 않아도 될것같다(?)
+      console.log('===>TOC render shouldComponentUpdate'
+      ,newProps.data
+      ,this.props.data
+      );
+      if(this.props.data === newProps){
+        return false;
+      }
+      return true;
+    }
     render (){
-      console.log('TOC render')
+      console.log('===>TOC render')
       var lists = []
       var data = this.props.data;
       var i = 0;
@@ -21,10 +32,10 @@ class TOC extends Component {
       }
       return (
         <nav>
-        <ul>
-          {lists}    
-        </ul>
-    </nav>
+          <ul>
+            {lists}    
+          </ul>
+        </nav>
       );
     }
   }
